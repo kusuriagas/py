e496171f0d84aa5d3bb66419b2a960e9bb00b100
EDISON <91626034+kusuriagas@users.noreply.github.com>
2021-12-09 18:49:39 -0500

    Update apuntes.py

--- apuntes.py
+++ apuntes.py
@@ -17,3 +17,30 @@ content[0]
 #copia y pegar
 pandas.read_clipboard()
 
+#dimensiones
+df_mundiales.shape
+out: (20, 10)
+n_filas=df_mundiales.shape[0]
+n_columnas=df_mundiales.shape[1]
+
+#informacion de la estructura
+df_mundiales.info()
+out:
+    <class 'pandas.core.frame.DataFrame'>
+    RangeIndex: 20 entries, 0 to 19
+    Data columns (total 10 columns):
+     #   Column          Non-Null Count  Dtype 
+    ---  ------          --------------  ----- 
+     0   Year            20 non-null     int64 
+     1   Country         20 non-null     object
+     2   Winner          20 non-null     object
+     3   Runners-Up      20 non-null     object
+     4   Third           20 non-null     object
+     5   Fourth          20 non-null     object
+     6   GoalsScored     20 non-null     int64 
+     7   QualifiedTeams  20 non-null     int64 
+     8   MatchesPlayed   20 non-null     int64 
+     9   Attendance      20 non-null     object
+    dtypes: int64(4), object(6)
+    memory usage: 1.7+ KB
+  
