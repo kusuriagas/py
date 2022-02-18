3388ba4a04972580f7d003dfae514ef94e706caa
EDISON <91626034+kusuriagas@users.noreply.github.com>
2022-02-18 18:55:14 -0500

    Update programacion.py

--- programacion.py
+++ programacion.py
@@ -26,14 +26,37 @@ str_date=dt.datetime.strptime(str_date,"%d/%m/%Y")
 print(str_date)
 
 
+# Funciones Anidadas
 
+def calculadora(a, b, operacion='sumar'):
+    # 1. Definir Función anidada
+    def sumar(a, b):
+        return a + b
 
+    def restar(a, b):
+        return a - b
 
+    # 2. Llamamos a la función anidada
+    if operacion == 'sumar':
+        print(f'Resultado sumar: {sumar(a, b)}')
+    elif operacion == 'restar':
+        print(f'Resultado restar: {restar(a, b)}')
 
+calculadora(5, 6)
+calculadora(4, 3, operacion='restar')
 
+#Manejo de archivos de python
+with open('C:\\Users\\user\\Desktop\\FileServer\\Northwind\\Categories.txt','w',encoding='utf8') as archivo:
+	archivo.write('agregamos texto al archivo linea 1 \n')
+	archivo.write('agregamos texto al archivo linea 2 \n')
+	archivo.write('agregamos texto al archivo linea 3 \n')
+	archivo.write('agregamos texto al archivo linea 4 \n')
+	archivo.write('agregamos texto al archivo linea 5 Televisión')
 
-
-
+with open('C:\\Users\\user\\Desktop\\FileServer\\Northwind\\Categories.txt','r',encoding='utf8') as file:
+	for linea in file:
+		print(linea)
+	print(file.readlines())#devuelve una lista
 
 
 
